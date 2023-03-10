b16abfed4e527aff6c3370d0a76ac59d12c3ce48
Martin Moss <martyzz1@gmail.com>
2023-03-10 14:30:25 +0000

    Make setup-env-vars eval so that we freeze any variables at the start

--- src/scripts/setup-env-vars.sh
+++ src/scripts/setup-env-vars.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2129
 
-echo "export FULL_IMAGE_TAG=\"${!FULL_IMAGE_TAG}\"" >> "$ENV_VAR_FILENAME"
-echo "export REPO=\"${!REPO}\"" >> "$ENV_VAR_FILENAME"
+eval echo "export FULL_IMAGE_TAG=\"${FULL_IMAGE_TAG}\"" >> "$ENV_VAR_FILENAME"
+eval echo "export REPO=\"${REPO}\"" >> "$ENV_VAR_FILENAME"
 echo "export GIT_TAG=\"$(git rev-parse HEAD)\"" >> "$ENV_VAR_FILENAME"
