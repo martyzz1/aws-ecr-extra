8c236df05b46cc6d466aaebc8f1179f7eaa62fba
Martin Moss <martyzz1@gmail.com>
2023-03-10 13:26:47 +0000

    change to bang interpreted

--- src/scripts/setup-env-vars.sh
+++ src/scripts/setup-env-vars.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2129
-echo "export FULL_IMAGE_TAG=\"$FULL_IMAGE_TAG\"" >> "$ENV_VAR_FILENAME"
-echo "export REPO=\"$REPO\"" >> "$ENV_VAR_FILENAME"
+
+echo "export FULL_IMAGE_TAG=\"${!FULL_IMAGE_TAG}\"" >> "$ENV_VAR_FILENAME"
+echo "export REPO=\"${!REPO}\"" >> "$ENV_VAR_FILENAME"
 echo "export GIT_TAG=\"$(git rev-parse HEAD)\"" >> "$ENV_VAR_FILENAME"
