6df380f19eff9fc74be2735c02d55776c489d284
Martin Moss <martyzz1@gmail.com>
2023-03-28 22:53:20 +0100

    Escape special chars for tags

--- src/scripts/setup-env-vars.sh
+++ src/scripts/setup-env-vars.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2129
 
-eval echo "export FULL_IMAGE_TAG=\"${FULL_IMAGE_TAG}\"" >> "$ENV_VAR_FILENAME"
-eval echo "export REPO=\"${REPO}\"" >> "$ENV_VAR_FILENAME"
+MY_FULL_IMAGE_TAG=$(eval echo "${FULL_IMAGE_TAG}" | sed 's/[^a-z0-9.-]/-/g' )
+MY_REPO=$(eval echo "${REPO}")
+
+echo "export FULL_IMAGE_TAG=\"${MY_FULL_IMAGE_TAG}\"" >> "$ENV_VAR_FILENAME"
+echo "export REPO=\"${MY_REPO}\"" >> "$ENV_VAR_FILENAME"
 echo "export GIT_TAG=\"$(git rev-parse HEAD)\"" >> "$ENV_VAR_FILENAME"
