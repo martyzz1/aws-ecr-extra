fb394aaea7fadcd6a66f8965aec6248082d8463b
Martin Moss <martyzz1@gmail.com>
2023-07-19 01:57:52 +0100

    Try to fix issue with disallowed chars in tags

--- src/scripts/tag-image-for-ecr.sh
+++ src/scripts/tag-image-for-ecr.sh
@@ -4,6 +4,7 @@ ORB_EVAL_REPO=$(eval echo "${ORB_EVAL_REPO}")
 ORB_EVAL_TAG=$(eval echo "${ORB_EVAL_TAG}")
 ORB_EVAL_REGION=$(eval echo "${ORB_EVAL_REGION}")
 ORB_VAL_ACCOUNT_URL="${!ORB_ENV_REGISTRY_ID}.dkr.ecr.${ORB_EVAL_REGION}.amazonaws.com"
+ORB_EVAL_TAGS=$(eval echo "${ORB_EVAL_TAG}" | tr '[:upper:]' '[:lower:]' | tr '/' '-')
 
 if [ -z "${!ORB_ENV_REGISTRY_ID}" ]; then
   echo "The registry ID is not found. Please add the registry ID as an environment variable in CicleCI before continuing."
@@ -19,8 +20,10 @@ else
 	echo "ORB_EVAL_REGION 3 is Weird"
 fi
 
-IFS="," read -ra DOCKER_TAGS <<< "${ORB_EVAL_TAG}"
+IFS="," read -ra DOCKER_TAGS <<< "${ORB_EVAL_TAGS}"
 for tag in "${DOCKER_TAGS[@]}"; do
-	echo "Tagging with tag \"$ORB_EVAL_REPO:${tag}\" \"$ORB_VAL_ACCOUNT_URL/$ORB_EVAL_REPO:${tag}\""
-	docker tag "$ORB_EVAL_REPO:${tag}" "$ORB_VAL_ACCOUNT_URL/$ORB_EVAL_REPO:${tag}"
+	my_tag=$(eval echo "${tag}" | sed 's/[^a-z0-9.-]/-/g' )
+
+	echo "Tagging with tag \"$ORB_EVAL_REPO:${my_tag}\" \"$ORB_VAL_ACCOUNT_URL/$ORB_EVAL_REPO:${my_tag}\""
+	docker tag "$ORB_EVAL_REPO:${my_tag}" "$ORB_VAL_ACCOUNT_URL/$ORB_EVAL_REPO:${my_tag}"
 done
